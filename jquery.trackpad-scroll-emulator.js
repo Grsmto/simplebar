7d378a04e98e08ee7d61ebf0828e5349ee88ba4a
Adrien Grsmto <grsmto@gmail.com>
2013-06-02 00:29:35 +0200

    Added "-webkit-overflow-scrolling" for mobile

--- jquery.trackpad-scroll-emulator.js
+++ jquery.trackpad-scroll-emulator.js
@@ -49,6 +49,10 @@
      * Initialize plugin
      */
     function init() {
+      if(hasOverflowScrolling) {
+        return;
+      }
+
       if ($el.hasClass('horizontal')){
         scrollDirection = 'horiz';
         scrollOffsetAttr = 'scrollLeft';
@@ -268,6 +272,46 @@
       if (options[hookName] !== undefined) {
         options[hookName].call(el);
       }
+    }
+
+    /**
+     * Check for mobile Overflow scrolling support
+     */
+    function hasOverflowScrolling() {
+        var prefixes = ['webkit', 'moz', 'o', 'ms'];
+        var div = document.createElement('div');
+        var body = document.getElementsByTagName('body')[0];
+        var hasIt = false;
+     
+        body.appendChild(div);
+     
+        for (var i = 0; i < prefixes.length; i++) {
+            var prefix = prefixes[i];
+            div.style[prefix + 'OverflowScrolling'] = 'touch';
+        }
+     
+        // And the non-prefixed property
+        div.style.overflowScrolling = 'touch';
+     
+        // Now check the properties
+        var computedStyle = window.getComputedStyle(div);
+     
+        // First non-prefixed
+        hasIt = !!computedStyle.overflowScrolling;
+     
+        // Now prefixed...
+        for (var i = 0; i < prefixes.length; i++) {
+            var prefix = prefixes[i];
+            if (!!computedStyle[prefix + 'OverflowScrolling']) {
+                hasIt = true;
+                break;
+            }
+        }
+     
+        // Cleanup old div elements
+        div.parentNode.removeChild(div);
+     
+        return hasIt;
     }
 
     init();
