05af251ae857fbcd9d2a03b8024bdff2fdf320b6
Adrien Denat <grsmto@gmail.com>
2018-03-19 22:30:37 -0300

    Fix demo with new setup

--- demo/index.js
+++ demo/index.js
@@ -4,13 +4,40 @@ import 'SimpleBar';
 
 
 ReactDOM.render(
-    <div className={"demo4"} data-simplebar>
-        <div className={"box"}>1</div>
-        <div className={"box"}>2</div>
-        <div className={"box"}>3</div>
-        <div className={"box"}>4</div>
-        <div className={"box"}>5</div>
-    </div>,
+    <section>
+        <section>
+            <div className="col demo1-col">
+                <div className="demo1" data-simplebar data-simplebar-auto-hide="false">
+                    {[...Array(50)].map((x, i) =>
+                        <p key={i} className="odd">Some content</p>
+                    )}
+                </div>
+            </div>
+            <div className="col demo2-col">
+                <h2>Default</h2>
+                <div className="demo1" data-simplebar>
+                    {[...Array(10)].map((x, i) =>
+                        <p key={i} className="odd">Some content</p>
+                    )}
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className="col demo4-col">
+                <div 
+                    className={"demo4"} 
+                    style={{ width: '200px', direction: 'rtl' }} 
+                    data-simplebar
+                >
+                    <div className={"box"}>1</div>
+                    <div className={"box"}>2</div>
+                    <div className={"box"}>3</div>
+                    <div className={"box"}>4</div>
+                    <div className={"box"}>5</div>
+                </div>
+            </div>
+        </section>
+    </section>,
     document.getElementById('root')
 );
 
--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: './'
   },
   module: {
     rules: [
