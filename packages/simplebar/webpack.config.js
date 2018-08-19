916678845bf4ab4c1874fb35a36d061439006e49
Adrien Denat <oi@adriendenat.com>
2018-08-19 19:44:39 -0300

    Tell Babel to scope config to the folder

--- packages/simplebar/webpack.config.js
+++ packages/simplebar/webpack.config.js
@@ -18,7 +18,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            cwd: __dirname
+          }
         }
       },
       {
