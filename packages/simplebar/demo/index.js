18ebecf8f25f3b07ae234c3d43271d616f72b42c
Adrien Denat <oi@adriendenat.com>
2018-09-04 20:23:41 -0300

    Add new Playground component on demos to control host element properties

--- packages/simplebar/demo/index.js
+++ packages/simplebar/demo/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import Select from 'react-select';
 import SimpleBar from 'simplebar-react';
 
+import 'react-select/dist/react-select.css';
 import 'simplebar/src/simplebar.css';
 
 function menuRenderer(params) {
@@ -12,7 +13,44 @@ function menuRenderer(params) {
     return <SimpleBar>{menu}</SimpleBar>;
 }
 
-ReactDOM.render(
+class Playground extends React.PureComponent {
+    state = {
+        height: '100%',
+        width: '100%'
+    }
+
+    render() {
+        return (
+            <Fragment>
+                {this.props.children(this.state)}
+                <div>
+                    <label>Width:</label>
+                    <Select
+                        onChange={(option) => this.setState({ width: option.value })}
+                        options={[
+                            { value: 'auto', label: 'auto' },
+                            { value: '100%', label: '100%' }
+                        ]}
+                        value={this.state.width}
+                    />
+                </div>
+                <div>
+                    <label>Height:</label>
+                    <Select
+                        onChange={(option) => this.setState({ height: option.value })}
+                        options={[
+                            { value: 'auto', label: 'auto' },
+                            { value: '100%', label: '100%' }
+                        ]}
+                        value={this.state.height}
+                    />
+                </div>
+            </Fragment>
+        );
+    }
+}
+    
+    ReactDOM.render(
     <section>
         <h1>Simplebar demo page</h1>
         <section>
@@ -50,11 +88,11 @@ ReactDOM.render(
                     data-simplebar
                     data-simplebar-direction="rtl"
                 >
-                    <div className={"box"}>1</div>
-                    <div className={"box"}>2</div>
-                    <div className={"box"}>3</div>
-                    <div className={"box"}>4</div>
-                    <div className={"box"}>5</div>
+                    <div className="box">1</div>
+                    <div className="box">2</div>
+                    <div className="box">3</div>
+                    <div className="box">4</div>
+                    <div className="box">5</div>
                 </div>
             </div>
         </section>
@@ -68,16 +106,86 @@ ReactDOM.render(
             </div>
             <div className="col">
                 <h2>Horizontal</h2>
+                <Playground>
+                    {({ height, width }) => (
+                        <div 
+                            style={{
+                                width: '200px',
+                                height: '200px'
+                            }}
+                        >
+                            <div
+                                data-simplebar
+                                className="demo4"
+                                style={{ height, width }}
+                            >
+                                <div className="box">1</div>
+                                <div className="box">2</div>
+                                <div className="box">3</div>
+                                <div className="box">4</div>
+                                <div className="box">5</div>
+                            </div>
+                        </div>
+                    )}
+                </Playground>
+            </div>
+        </section>
+        <section>
+            <div className="col">
+                <h2>Horizontal</h2>
                 <div
                     className={"demo4"}
                     style={{ width: '200px' }}
                     data-simplebar
                 >
-                    <div className={"box"}>1</div>
-                    <div className={"box"}>2</div>
-                    <div className={"box"}>3</div>
-                    <div className={"box"}>4</div>
-                    <div className={"box"}>5</div>
+                    <div className="box">1</div>
+                    <div className="box">2</div>
+                    <div className="box">3</div>
+                    <div className="box">4</div>
+                    <div className="box">5</div>
+                </div>
+            </div>
+            <div className="col">
+                <h2>Horizontal</h2>
+                <div
+                    className={"demo4"}
+                    style={{ width: '200px' }}
+                >
+                    <div className="box">1</div>
+                    <div className="box">2</div>
+                    <div className="box">3</div>
+                    <div className="box">4</div>
+                    <div className="box">5</div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className="col">
+                <h2>Both axis</h2>
+                <div
+                    className={"demo-both-axis"}
+                    data-simplebar
+                >
+                    <div className="box">1</div>
+                </div>
+            </div>
+            <div className="col">
+                <h2>Both axis + padding</h2>
+                <div
+                    className={"demo-both-axis demo-both-axis--padding"}
+                    data-simplebar
+                >
+                    <div className="box">1</div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className="col">
+                <h2>Both axis + padding</h2>
+                <div
+                    className={"demo-both-axis demo-both-axis--padding"}
+                >
+                    <div className="box">1</div>
                 </div>
             </div>
         </section>
@@ -86,3 +194,4 @@ ReactDOM.render(
 );
 
 // var demo5 = new SimpleBar(document.getElementById('demo5'));
+
