99da26e8c17002f468678f3af6ee246b91761544
Adrien Denat <adrien.denat@gmail.com>
2018-11-19 23:49:57 -0200

    chore: add new tests

--- packages/simplebar/test/simplebar.test.js
+++ packages/simplebar/test/simplebar.test.js
@@ -4,7 +4,7 @@ beforeEach(() => {
   jest.resetModules();
 
   // Set up our document body
-  document.body.innerHTML = '<div id="simplebar"></div>';
+  document.body.innerHTML = '<div id="simplebar" data-simplebar-auto-hide="true"></div>';
 });
 
 test('should call constructor', () => {
@@ -21,4 +21,45 @@ test('should return the scroll element', () => {
   const scrollElement = simpleBar.getScrollElement();
 
   expect(scrollElement).toBe(simpleBar.contentEl);
+});
+
+test("should unmount SimpleBar", () => {
+  const simpleBar = new SimpleBar(document.getElementById("simplebar"));
+
+  simpleBar.unMount();
+
+  expect(simpleBar.el.SimpleBar).toBeNull();
+});
+
+test("should return the element options", () => {
+  const simpleBar = new SimpleBar(document.getElementById('simplebar'));
+
+  expect(SimpleBar.getElOptions(simpleBar.el)).toEqual({ autoHide: true });
+});
+
+test("should know if element is a child node", () => {
+  const simpleBar = new SimpleBar(document.getElementById("simplebar"));
+
+  expect(simpleBar.isChildNode(simpleBar.contentEl)).toBeTruthy();
+  expect(simpleBar.isChildNode(document.body)).toBeFalsy();
+});
+
+test("mouse should be within bounds", () => {
+  const simpleBar = new SimpleBar(document.getElementById("simplebar"));
+
+  simpleBar.mouseX = 20;
+  simpleBar.mouseY = 20;
+
+  const isWithinBounds = simpleBar.isWithinBounds({
+    bottom: 110,
+    height: 100,
+    left: 10,
+    right: 110,
+    top: 10,
+    width: 100,
+    x: 10,
+    y: 10,
+  });
+
+  expect(isWithinBounds).toBeTruthy();
 });
