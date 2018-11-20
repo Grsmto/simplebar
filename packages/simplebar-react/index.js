5d5acd116793392ae6788ca69bfbce6180ca00c0
Adrien Denat <adrien.denat@gmail.com>
2018-11-19 22:49:56 -0200

    chore: update react and vue plugin after breaking change

--- packages/simplebar-react/index.js
+++ packages/simplebar-react/index.js
@@ -3,29 +3,25 @@ import PropTypes from 'prop-types';
 import 'simplebar';
 
 export default function SimpleBar({ children, ...options }) {
-  return (
-    <div data-simplebar {...options}>
+  return <div data-simplebar {...options}>
       <div className="simplebar-wrapper">
         <div className="simplebar-height-auto-observer-wrapper">
-          <div className="simplebar-height-auto-observer"></div>
+          <div className="simplebar-height-auto-observer" />
         </div>
         <div className="simplebar-mask">
           <div className="simplebar-offset">
-            <div className="simplebar-content">
-              {children}
-            </div>
+            <div className="simplebar-content">{children}</div>
           </div>
         </div>
         <div className="simplebar-placeholder" />
       </div>
-      <div className="simplebar-track horizontal">
+      <div className="simplebar-track simplebar-horizontal">
         <div className="simplebar-scrollbar" />
       </div>
-      <div className="simplebar-track vertical">
+      <div className="simplebar-track simplebar-vertical">
         <div className="simplebar-scrollbar" />
       </div>
-    </div>
-  );
+    </div>;
 }
 
 SimpleBar.propTypes = {
--- packages/simplebar-vue/index.vue
+++ packages/simplebar-vue/index.vue
@@ -2,11 +2,25 @@
   <div
     data-simplebar
   >
-    <div class="simplebar-scroll-content">
-      <div class="simplebar-content">
-        <slot></slot>
+    <div class="simplebar-wrapper">
+        <div class="simplebar-height-auto-observer-wrapper">
+          <div class="simplebar-height-auto-observer" />
+        </div>
+        <div class="simplebar-mask">
+          <div class="simplebar-offset">
+            <div class="simplebar-content">
+              <slot></slot>
+            </div>
+          </div>
+        </div>
+        <div class="simplebar-placeholder" />
       </div>
-    </div>
+      <div class="simplebar-track simplebar-horizontal">
+        <div class="simplebar-scrollbar" />
+      </div>
+      <div class="simplebar-track simplebar-vertical">
+        <div class="simplebar-scrollbar" />
+      </div>
   </div>
 </template>
 
