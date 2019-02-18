54c0e4aff0b5fcb704787e778f071feb47617e7a
Adrien Denat <adrien.denat@gmail.com>
2019-02-18 17:35:05 -0300

    chore: fix test env after Jest upgrade

--- packages/simplebar/jest-puppeteer.config.js
+++ packages/simplebar/jest-puppeteer.config.js
@@ -1,9 +1,10 @@
 module.exports = {
     launch: {
         headless: process.env.CI === 'true',
+        devtools: process.env.CI === 'false'
     },
     server: {
         command: 'webpack-dev-server --port 8091 --mode=development',
-        port: 8091
+        port: 8091,
     }
 }
