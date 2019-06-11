f4688c52f39682a715bc34a0c06da9901404c0bc
Amir Abdullaev <f278f1b2@tuta.io>
2019-06-11 19:06:06 +0300

    add forward ref to react component

--- packages/simplebar-react/index.js
+++ packages/simplebar-react/index.js
@@ -2,45 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import 'simplebar';
 
-export default function SimpleBar({
-  children,
-  scrollableNodeProps,
-  ...options
-}) {
-  return (
-    <div data-simplebar {...options}>
-      <div className="simplebar-wrapper">
-        <div className="simplebar-height-auto-observer-wrapper">
-          <div className="simplebar-height-auto-observer" />
-        </div>
-        <div className="simplebar-mask">
-          <div className="simplebar-offset">
-            <div className="simplebar-content-wrapper">
-              <div
-                {...scrollableNodeProps}
-                className={`simplebar-content${
-                  scrollableNodeProps && scrollableNodeProps.className
-                    ? ` ${scrollableNodeProps.className}`
-                    : ''
-                }`}
-              >
-                {children}
+const SimpleBar = React.forwardRef(
+  ({ children, scrollableNodeProps, ...options }, ref) => {
+    return (
+      <div data-simplebar {...options}>
+        <div className="simplebar-wrapper">
+          <div className="simplebar-height-auto-observer-wrapper">
+            <div className="simplebar-height-auto-observer" />
+          </div>
+          <div className="simplebar-mask">
+            <div className="simplebar-offset">
+              <div className="simplebar-content-wrapper" ref={ref}>
+                <div
+                  {...scrollableNodeProps}
+                  className={`simplebar-content${
+                    scrollableNodeProps && scrollableNodeProps.className
+                      ? ` ${scrollableNodeProps.className}`
+                      : ''
+                  }`}
+                >
+                  {children}
+                </div>
               </div>
             </div>
           </div>
+          <div className="simplebar-placeholder" />
         </div>
-        <div className="simplebar-placeholder" />
+        <div className="simplebar-track simplebar-horizontal">
+          <div className="simplebar-scrollbar" />
+        </div>
+        <div className="simplebar-track simplebar-vertical">
+          <div className="simplebar-scrollbar" />
+        </div>
       </div>
-      <div className="simplebar-track simplebar-horizontal">
-        <div className="simplebar-scrollbar" />
-      </div>
-      <div className="simplebar-track simplebar-vertical">
-        <div className="simplebar-scrollbar" />
-      </div>
-    </div>
-  );
-}
+    );
+  }
+);
 
 SimpleBar.propTypes = {
   children: PropTypes.node
 };
+
+export default SimpleBar;
