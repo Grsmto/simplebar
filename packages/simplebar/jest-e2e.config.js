2e9295b3d29166cedb4ae18c5d22333a6da94107
qiurenbo <qiurenbo@outlook.com>
2019-09-11 13:35:03 +0800

    fix: fix jest  timeout

--- packages/simplebar/jest-e2e.config.js
+++ packages/simplebar/jest-e2e.config.js
@@ -1,13 +1,10 @@
 module.exports = {
-    preset: 'jest-puppeteer',
-    testRegex: '^.+\\.test\\.e2e\\.js$',
-    'transform': {
-        "^.+\\.js?$": "babel-jest"
-    },
-    'transformIgnorePatterns': [
-        "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$"
-    ],
-    'moduleFileExtensions': [
-        "js"
-    ]
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  preset: 'jest-puppeteer',
+  testRegex: '^.+\\.test\\.e2e\\.js$',
+  transform: {
+    '^.+\\.js?$': 'babel-jest'
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
+  moduleFileExtensions: ['js']
 };
