769ab9f5756e752c840293c30a4d083c37aebdd1
qiurenbo <qiurenbo@outlook.com>
2019-09-11 08:51:46 +0800

    chore: set default jest timeout

--- packages/simplebar/jest-e2e.config.js
+++ packages/simplebar/jest-e2e.config.js
@@ -1,13 +1,10 @@
 module.exports = {
-    preset: 'jest-puppeteer',
-    testRegex: '^.+\\.test\\.e2e\\.js$',
-    'transform': {
-        "^.+\\.js?$": "babel-jest"
-    },
-    'transformIgnorePatterns': [
-        "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$"
-    ],
-    'moduleFileExtensions': [
-        "js"
-    ]
+  setupTestFrameworkScriptFile: './jest.setup.js',
+  preset: 'jest-puppeteer',
+  testRegex: '^.+\\.test\\.e2e\\.js$',
+  transform: {
+    '^.+\\.js?$': 'babel-jest'
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
+  moduleFileExtensions: ['js']
 };
