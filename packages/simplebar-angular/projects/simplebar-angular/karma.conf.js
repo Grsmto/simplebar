19b4f46cd5ad3605388528cee51a07a096a7326d
Adrien Denat <adrien.denat@gmail.com>
2019-09-13 17:45:13 -0300

    chore: try to fix Angular tests

--- packages/simplebar-angular/projects/simplebar-angular/karma.conf.js
+++ packages/simplebar-angular/projects/simplebar-angular/karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -26,7 +26,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
