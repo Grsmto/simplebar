71da27143c9aa30d69d3b4894977c4d7b7ffb900
Adrien Denat <adrien.denat@gmail.com>
2019-10-01 19:07:39 -0300

    feat: add favicon

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
