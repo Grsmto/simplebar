149d2f1394910c7e3eaf143a5b1cd317cd145792
Adrien Denat <adrien.denat@gmail.com>
2019-10-16 15:32:48 -0300

    feat(react): add support for ref (fix #369)

--- packages/simplebar-react/index.js
+++ packages/simplebar-react/index.js
@@ -34,91 +34,105 @@ const getOptions = function(obj) {
   return options;
 };
 
-export default function SimpleBar({
-  children,
-  scrollableNodeProps = {},
-  ...otherProps
-}) {
-  const elRef = useRef();
-  const scrollableNodeRef = useRef();
-  const contentNodeRef = useRef();
-  let options = {};
-  let rest = {};
-  let deprecatedOptions = [];
+const SimpleBar = React.forwardRef(
+  ({ children, scrollableNodeProps = {}, ...otherProps }, ref) => {
+    let instance;
+    const elRef = useRef({});
+    const scrollableNodeRef = useRef();
+    const contentNodeRef = useRef();
+    let options = {};
+    let rest = {};
+    let deprecatedOptions = [];
 
-  Object.keys(otherProps).forEach(key => {
-    if (Object.prototype.hasOwnProperty.call(SimpleBarJS.defaultOptions, key)) {
-      options[key] = otherProps[key];
-    } else if (key.match(/data-simplebar-(.+)/)) {
-      deprecatedOptions.push({
-        name: key,
-        value: otherProps[key]
-      });
-    } else {
-      rest[key] = otherProps[key];
-    }
-  });
+    Object.keys(otherProps).forEach(key => {
+      if (
+        Object.prototype.hasOwnProperty.call(SimpleBarJS.defaultOptions, key)
+      ) {
+        options[key] = otherProps[key];
+      } else if (key.match(/data-simplebar-(.+)/)) {
+        deprecatedOptions.push({
+          name: key,
+          value: otherProps[key]
+        });
+      } else {
+        rest[key] = otherProps[key];
+      }
+    });
 
-  if (deprecatedOptions.length) {
-    console.warn(`simplebar-react: this way of passing options is deprecated. Pass it like normal props instead:
+    if (deprecatedOptions.length) {
+      console.warn(`simplebar-react: this way of passing options is deprecated. Pass it like normal props instead:
         'data-simplebar-auto-hide="false"' —> 'autoHide="false"'
       `);
-  }
-
-  useEffect(() => {
-    const scrollableNodeRef = scrollableNodeProps.ref || scrollableNodeRef;
-
-    if (elRef.current) {
-      new SimpleBarJS(elRef.current, {
-        ...getOptions(deprecatedOptions),
-        ...options,
-        ...(scrollableNodeRef && {
-          scrollableNode: scrollableNodeRef.current
-        }),
-        ...(contentNodeRef.current && {
-          contentNode: contentNodeRef.current
-        })
-      });
     }
-  });
 
-  return (
-    <div ref={elRef} {...rest}>
-      <div className="simplebar-wrapper">
-        <div className="simplebar-height-auto-observer-wrapper">
-          <div className="simplebar-height-auto-observer" />
-        </div>
-        <div className="simplebar-mask">
-          <div className="simplebar-offset">
-            {typeof children === 'function' ? (
-              children({ scrollableNodeRef, contentNodeRef })
-            ) : (
-              <div
-                {...scrollableNodeProps}
-                className={`simplebar-content-wrapper${
-                  scrollableNodeProps.className
-                    ? ` ${scrollableNodeProps.className}`
-                    : ''
-                }`}
-              >
-                <div className="simplebar-content">{children}</div>
-              </div>
-            )}
+    useEffect(() => {
+      const scrollableNodeRef = scrollableNodeProps.ref || scrollableNodeRef;
+
+      if (elRef.current) {
+        instance = new SimpleBarJS(elRef.current, {
+          ...getOptions(deprecatedOptions),
+          ...options,
+          ...(scrollableNodeRef && {
+            scrollableNode: scrollableNodeRef.current
+          }),
+          ...(contentNodeRef.current && {
+            contentNode: contentNodeRef.current
+          })
+        });
+
+        if (ref) {
+          ref.current = instance;
+        }
+      }
+
+      return () => {
+        instance.unMount();
+        instance = null;
+      };
+    });
+
+    return (
+      <div ref={elRef} {...rest}>
+        <div className="simplebar-wrapper">
+          <div className="simplebar-height-auto-observer-wrapper">
+            <div className="simplebar-height-auto-observer" />
           </div>
+          <div className="simplebar-mask">
+            <div className="simplebar-offset">
+              {typeof children === 'function' ? (
+                children({ scrollableNodeRef, contentNodeRef })
+              ) : (
+                <div
+                  {...scrollableNodeProps}
+                  className={`simplebar-content-wrapper${
+                    scrollableNodeProps.className
+                      ? ` ${scrollableNodeProps.className}`
+                      : ''
+                  }`}
+                >
+                  <div className="simplebar-content">{children}</div>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="simplebar-placeholder" />
         </div>
-        <div className="simplebar-placeholder" />
+        <div className="simplebar-track simplebar-horizontal">
+          <div className="simplebar-scrollbar" />
+        </div>
+        <div className="simplebar-track simplebar-vertical">
+          <div className="simplebar-scrollbar" />
+        </div>
       </div>
-      <div className="simplebar-track simplebar-horizontal">
-        <div className="simplebar-scrollbar" />
-      </div>
-      <div className="simplebar-track simplebar-vertical">
-        <div className="simplebar-scrollbar" />
-      </div>
-    </div>
-  );
-}
+    );
+  }
+);
+
+SimpleBar.displayName = 'SimpleBar';
 
 SimpleBar.propTypes = {
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
   scrollableNodeProps: PropTypes.object
 };
+
+export default SimpleBar;
--- packages/simplebar-react/tests/index.test.js
+++ packages/simplebar-react/tests/index.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, waitForElement } from '@testing-library/react';
-
 import SimpleBar from '../dist/simplebar-react';
 
 test('renders without crashing', () => {
@@ -55,8 +54,41 @@ test('renders with scrollableNodeProps', async () => {
       ))}
     </SimpleBar>
   );
+
   await waitForElement(() =>
     // getByTestId throws an error if it cannot find an element
     getByTestId('scrollable-node-props')
   );
+});
+
+test('renders with ref', async () => {
+  const ref = React.createRef();
+
+  render(
+    <SimpleBar ref={ref}>
+      {[...Array(5)].map((x, i) => (
+        <p key={i}>Some content</p>
+      ))}
+    </SimpleBar>
+  );
+
+  expect(ref.current).toBeDefined();
+});
+
+test('works on unmount', async () => {
+  const ref = React.createRef();
+
+  const { unmount } = render(
+    <SimpleBar ref={ref}>
+      {[...Array(5)].map((x, i) => (
+        <p key={i}>Some content</p>
+      ))}
+    </SimpleBar>
+  );
+
+  jest.spyOn(ref.current, 'unMount');
+
+  unmount();
+
+  expect(ref.current.unMount).toHaveBeenCalled();
 });
