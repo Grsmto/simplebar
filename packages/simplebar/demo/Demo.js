87f9615c212e648ba60dcd6fc7c2c9e4a3fae886
Jack Armstrong <jackarms@fb.com>
2019-11-18 20:26:59 -0800

    clean up example

--- packages/simplebar/demo/Demo.js
+++ packages/simplebar/demo/Demo.js
@@ -380,7 +380,7 @@ class IFrame extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      ifameLoaded: false
+      iframeLoaded: false
     };
   }
 
@@ -389,8 +389,6 @@ class IFrame extends React.Component {
       <iframe
         ref={ref => {
           if (ref && ref.contentWindow && !this.state.iframeLoaded) {
-            console.log('set ref');
-            console.log(ref.contentWindow.document.body);
             this.contentRef = ref.contentWindow.document.body;
             this.setState({
               iframeLoaded: true
