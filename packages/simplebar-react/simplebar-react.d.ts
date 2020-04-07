8a9dc4bf54832c705408bb04d6bd1cafec104669
Rodrigo Almeida <djrodrigoalmeida@yahoo.com.br>
2020-04-07 20:27:54 -0300

    Add type definitions to fix typescript project
    Added type definitions to "autoHide" and "forceVisible" to fix erros on use it on typescript projects

--- packages/simplebar-react/simplebar-react.d.ts
+++ packages/simplebar-react/simplebar-react.d.ts
@@ -4,6 +4,8 @@ declare module 'simplebar-react' {
   interface Props extends React.HTMLAttributes<HTMLElement> {
     scrollableNodeProps?: object;
     options?: object;
+    autoHide?: boolean;
+    forceVisible?: string
   }
 
   export default class SimpleBar extends React.Component<Props> {}
