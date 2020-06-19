c34b4e023e706ce2a771831cc1a1c1fb4b42b3d8
Adrien Denat <adrien.denat@gmail.com>
2020-06-18 23:05:24 -0300

    fix(core): fix webpack config overriding babelrc file

--- packages/simplebar/webpack.config.js
+++ packages/simplebar/webpack.config.js
@@ -20,13 +20,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', { modules: false }],
-              '@babel/preset-react'
-            ]
-          }
+          loader: 'babel-loader'
         }
       },
       {
