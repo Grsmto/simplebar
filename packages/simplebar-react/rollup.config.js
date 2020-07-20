b6917c7313d504b19cc5a5337619cd21f7cf3e60
Adrien Denat <adrien.denat@gmail.com>
2020-07-20 01:18:35 -0300

    chore(react): use right rollup plugins

--- packages/simplebar-react/rollup.config.js
+++ packages/simplebar-react/rollup.config.js
@@ -1,6 +1,6 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
 import license from 'rollup-plugin-license';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
