f450582e6f950e241c4137deb5f6e37d01230da2
Adrien Denat <adrien.denat@gmail.com>
2020-08-29 02:34:00 -0300

    chore(core): compile core to umd so it can be used in test env by other packages

--- packages/simplebar-core/rollup.config.js
+++ packages/simplebar-core/rollup.config.js
@@ -1,3 +1,5 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import license from 'rollup-plugin-license';
 import pkg from './package.json';
@@ -15,34 +17,47 @@ const licence = {
 
 export default [
   // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
   {
     input: 'src/index.js',
-    external: (id) => {
-      if (
-        Object.keys(pkg.dependencies).find((dep) => id === dep) ||
-        id.match(/(core-js).+/)
-      ) {
-        return true;
-      }
-
-      return false;
-    },
-    output: {
-      file: pkg.main,
-      format: 'esm',
-      sourcemap: true,
-    },
+    external: Object.keys(pkg.dependencies),
+    output: [
+      {
+        file: pkg.main,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
     plugins: [
       babel({
         exclude: ['/**/node_modules/**'],
         babelHelpers: 'runtime',
         plugins: [['@babel/plugin-transform-runtime', { useESModules: true }]],
       }),
+      license(licence),
+    ],
+  },
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        name: 'SimpleBar',
+        file: pkg.main,
+        format: 'umd',
+        sourcemap: true,
+        globals: {
+          'lodash-es': '_',
+        },
+      },
+    ],
+    plugins: [
+      babel({
+        exclude: ['/**/node_modules/**'],
+        babelHelpers: 'runtime',
+        plugins: ['@babel/plugin-transform-runtime'],
+      }),
+      resolve(), // so Rollup can find dependencies
+      commonjs(), // so Rollup can convert dependencies to an ES module
       license(licence),
     ],
   },
