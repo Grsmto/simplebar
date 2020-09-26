7d2d7f6c82c0da5ebdc1585f557385a2bd311063
Adrien Denat <adrien.denat@gmail.com>
2020-09-26 04:34:41 +0100

    chore(core): add safety check on unmount breaking SSR in some situation (ref #474)

--- packages/simplebar-core/src/index.js
+++ packages/simplebar-core/src/index.js
@@ -855,10 +855,15 @@ export default class SimpleBar {
     this.el.removeEventListener('mousemove', this.onMouseMove);
     this.el.removeEventListener('mouseleave', this.onMouseLeave);
 
-    this.contentWrapperEl.removeEventListener('scroll', this.onScroll);
+    if (this.contentWrapperEl) {
+      this.contentWrapperEl.removeEventListener('scroll', this.onScroll);
+    }
+
     elWindow.removeEventListener('resize', this.onWindowResize);
 
-    this.mutationObserver.disconnect();
+    if (this.mutationObserver) {
+      this.mutationObserver.disconnect();
+    }
 
     if (this.resizeObserver) {
       this.resizeObserver.disconnect();
