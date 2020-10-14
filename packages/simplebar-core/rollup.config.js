951f18192e9da002c517444117338427597b045b
Adrien Denat <adrien.denat@gmail.com>
2020-10-14 23:33:22 +0100

    fix(core): fix UMD build bundled with incompatible lodash-es

--- packages/simplebar-core/rollup.config.js
+++ packages/simplebar-core/rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-import { getExternals } from '../../rollup.config';
+import { getExternals, babelConfig } from '../../rollup.config';
 import pkg from './package.json';
 
 export default [
@@ -37,15 +37,19 @@ export default [
           'can-use-dom': 'canUseDOM',
           'lodash-es': '_',
         },
+        paths: {
+          // Lodash-es won't work for UMD build, so we alias to lodash
+          'lodash-es': 'lodash',
+        },
       },
     ],
     plugins: [
-      commonjs(), // so Rollup can convert dependencies to an ES module
-      resolve(), // so Rollup can find dependencies
       babel({
-        babelHelpers: 'runtime',
+        ...babelConfig,
         plugins: ['@babel/plugin-transform-runtime'],
       }),
+      resolve(),
+      commonjs(),
     ],
   },
 ];
