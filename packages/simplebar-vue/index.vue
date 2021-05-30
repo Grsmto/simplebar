01a16dd1c054ad194df5655333e8cacfb199c7d3
Dmitry Sviridenko <letehaha@gmail.com>
2021-05-30 13:53:59 +0300

    [Vue package] Feat: handle scroll listener

--- packages/simplebar-vue/index.vue
+++ packages/simplebar-vue/index.vue
@@ -1,28 +1,30 @@
 <template>
-  <div
-    ref="element"
-  >
+  <div ref="element">
     <div class="simplebar-wrapper">
-        <div class="simplebar-height-auto-observer-wrapper">
-          <div class="simplebar-height-auto-observer" />
-        </div>
-        <div class="simplebar-mask">
-          <div class="simplebar-offset">
-            <div class="simplebar-content-wrapper" ref="scrollElement">
-              <div class="simplebar-content" ref="contentElement">
-                <slot></slot>
-              </div>
+      <div class="simplebar-height-auto-observer-wrapper">
+        <div class="simplebar-height-auto-observer" />
+      </div>
+      <div class="simplebar-mask">
+        <div class="simplebar-offset">
+          <div
+            class="simplebar-content-wrapper"
+            ref="scrollElement"
+            @scroll="$listeners.scroll"
+          >
+            <div class="simplebar-content" ref="contentElement">
+              <slot></slot>
             </div>
           </div>
         </div>
-        <div class="simplebar-placeholder" />
       </div>
-      <div class="simplebar-track simplebar-horizontal">
-        <div class="simplebar-scrollbar" />
-      </div>
-      <div class="simplebar-track simplebar-vertical">
-        <div class="simplebar-scrollbar" />
-      </div>
+      <div class="simplebar-placeholder" />
+    </div>
+    <div class="simplebar-track simplebar-horizontal">
+      <div class="simplebar-scrollbar" />
+    </div>
+    <div class="simplebar-track simplebar-vertical">
+      <div class="simplebar-scrollbar" />
+    </div>
   </div>
 </template>
 
