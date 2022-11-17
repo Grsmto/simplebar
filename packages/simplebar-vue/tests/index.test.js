fe2454a48c605e0124f32df9b266f2fffda2b4ed
Renato de Leão <renatodeleao@gmail.com>
2022-11-17 13:12:42 +0000

    test: setup vue isomorphic testing capabilities
    We can now run same tests as vue 2.6, 2.7 (composition-apo) and 3.x from a single file test file. Note we still need to adjust the mountingOptions signature because vue-test-utils has some breaking changes from 1 to 2. If tests get better (more complext) an isomorphic wrapper around mount/shallowMount should be added to test-utils/.js.
    
    - adds swap-vue.js script and vue-demi: this is straight from vuelidate codebase
    - adds aliased vue versions of core, compilers and vue-jest. swap-vue and vue demi will set the proper versions beforehand
    - using isVue3 flag from vue-demi we conditionally re-export @vue-test/utils from the appropriate version
    - called html() before snapshots, because jest-serializer-vue is not yet compatible with vue3 https://github.com/eddyerburgh/jest-serializer-vue/pull/56
    -

--- packages/simplebar-vue/tests/index.test.js
+++ packages/simplebar-vue/tests/index.test.js
@@ -1,31 +1,46 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, destroyWrapper, isVue3 } from './test-utils';
 import simplebar from '../index.vue';
+import SimpleBar from 'simplebar';
 
 describe('simplebar', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallowMount(simplebar);
-    expect(wrapper).toMatchSnapshot();
-  });
+  describe('snapshots', () => {
+    /**
+     * jest-serializer-vue is not compatible with vue3 yet, so you
+     * cannot pass the wrapper object directly. Call .html() before
+     * @see {@link https://github.com/eddyerburgh/jest-serializer-vue/pull/56}
+     */
+    it('renders without crashing', () => {
+      const wrapper = shallowMount(simplebar);
+      expect(wrapper.html()).toMatchSnapshot();
 
-  it('renders with options', () => {
-    const wrapper = shallowMount(simplebar, {
-      propsData: { 'data-simplebar-auto-hide': 'false' }
+      destroyWrapper(wrapper);
     });
-    expect(wrapper).toMatchSnapshot();
-  });
 
-  it('renders with default slot', () => {
-    const wrapper = shallowMount(simplebar, {
-      slots: {
-        default: '<div class="inner-content" />'
-      }
+    it('renders with options', () => {
+      const wrapper = shallowMount(simplebar, {
+        attrs: { 'data-simplebar-auto-hide': 'false' }
+      });
+      expect(wrapper.html()).toMatchSnapshot();
     });
-    expect(wrapper).toMatchSnapshot();
+
+    it('renders with default slot', () => {
+      const wrapper = shallowMount(simplebar, {
+        slots: {
+          default: '<div class="inner-content" />'
+        }
+      });
+      expect(wrapper.html()).toMatchSnapshot();
+    });
   });
 
   it('can access SimpleBar instance', () => {
     const wrapper = shallowMount(simplebar);
-    expect(wrapper.vm.SimpleBar).toBeDefined();
+    expect(wrapper.vm.SimpleBar).toBeInstanceOf(SimpleBar);
+  });
+
+  it('can access root element ref property', () => {
+    const wrapper = shallowMount(simplebar);
+    expect(wrapper.element).toEqual(wrapper.vm.$refs.element);
   });
 
   it('can access scrollElement property', () => {
@@ -44,9 +59,16 @@ describe('simplebar', () => {
     expect(scrollElement).toEqual(wrapper.find('.simplebar-content').element);
   });
 
-  it('works with options as attribute', () => {
+  it('works with options as  data attributes', () => {
     const wrapper = shallowMount(simplebar, {
-      propsData: { 'data-simplebar-auto-hide': 'false' }
+      attrs: { 'data-simplebar-auto-hide': 'false' }
+    });
+    expect(wrapper.vm.SimpleBar.options.autoHide).toEqual(false);
+  });
+
+  it('works with options as props', () => {
+    const wrapper = shallowMount(simplebar, {
+      [isVue3 ? 'props' : 'propsData']: { autoHide: false }
     });
     expect(wrapper.vm.SimpleBar.options.autoHide).toEqual(false);
   });
