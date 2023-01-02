d4168a0b53d047e709419f6682c0b318ec3484b2
Adrien Denat <oi@adriendenat.com>
2023-01-02 22:18:19 +0000

    chore(core): bring back PR #587 from Vikkivi/feature/keyboard_accessibility

--- packages/simplebar-core/src/index.js
+++ packages/simplebar-core/src/index.js
@@ -185,6 +185,8 @@ export default class SimpleBar {
     if (canUseDOM) {
       this.initDOM();
 
+      this.setAccessibilityAttributes();
+
       this.rtlHelpers = SimpleBar.getRtlHelpers();
       this.scrollbarWidth = SimpleBar.getScrollbarWidth();
 
@@ -301,6 +303,14 @@ export default class SimpleBar {
     }
 
     this.el.setAttribute('data-simplebar', 'init');
+  }
+
+  setAccessibilityAttributes() {
+    const ariaLabel = this.options.ariaLabel || 'scrollable content';
+
+    this.contentWrapperEl.setAttribute('tabindex', '0');
+    this.contentWrapperEl.setAttribute('role', 'region');
+    this.contentWrapperEl.setAttribute('aria-label', ariaLabel);
   }
 
   initListeners() {
