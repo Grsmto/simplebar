e8e8c4c82dfc434292c8677e288b2ae9ab35f21d
Adrien Denat <oi@adriendenat.com>
2023-01-06 18:45:40 +0000

    chore(examples): upgrade to latest Webpack

--- packages/examples/webpack.config.js
+++ packages/examples/webpack.config.js
@@ -1,27 +1,18 @@
-const path = require('path');
-
 module.exports = {
   entry: './index.js',
   devtool: 'cheap-module-source-map',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './'),
   },
   devServer: {
-    contentBase: './',
     port: 8090,
-    inline: true,
-    // host: "0.0.0.0",
-    // allowedHosts: ['.local']
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js?$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: 'babel-loader',
       },
       {
         test: /\.css$/,
