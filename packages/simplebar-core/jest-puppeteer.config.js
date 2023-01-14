305f906f066a6f13975a86a44487b9048ed385b7
Adrien Denat <oi@adriendenat.com>
2023-01-14 15:07:17 +0000

    chore(core): fix Browserstack build names

--- packages/simplebar-core/jest-puppeteer.config.js
+++ packages/simplebar-core/jest-puppeteer.config.js
@@ -3,8 +3,12 @@ const caps = {
   browser_version: 'latest', // We support v83 and above. You can choose `latest`, `latest-beta`, `latest-1`, `latest-2` and so on, in this capability
   os: 'os x',
   os_version: 'big sur',
-  build: 'puppeteer-build-1',
-  name: 'My first Puppeteer test', // The name of your test and build. See browserstack.com/docs/automate/puppeteer/organize tests for more details
+  project: 'simplebar',
+  build:
+    process.env.CI === 'true'
+      ? `${process.env.TRAVIS_BRANCH}-${process.env.TRAVIS_COMMIT}`
+      : 'local-tests',
+  name: 'SimpleBar examples Puppeteer test', // The name of your test and build. See browserstack.com/docs/automate/puppeteer/organize tests for more details
   'browserstack.username': process.env.BROWSERSTACK_USERNAME,
   'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY,
   'browserstack.local': 'true',
