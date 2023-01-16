ed29c034ed995879cea2234fd126f24aeda5001f
Travis CI User <travis@example.org>
2023-01-16 12:51:17 +0000

    Updates

---
+++ component---src-pages-404-js-0edf6e7a82de8614f412.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksimplebar_website=self.webpackChunksimplebar_website||[]).push([[883],{4431:function(e,t,n){n.r(t);var l=n(2784),s=n(6967),u=n(9779);t.default=function(){return l.createElement(s.Z,null,l.createElement(u.Z,{title:"404: Not found"}),l.createElement("h1",null,"NOT FOUND"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0edf6e7a82de8614f412.js.map
