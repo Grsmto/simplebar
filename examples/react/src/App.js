aba5642f7399a33e1954bdc8333b294ab1e6baff
Adrien Denat <oi@adriendenat.com>
2023-02-25 14:04:52 +0000

    chore: update react example deps

--- examples/react/src/App.js
+++ examples/react/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import SimpleBar from 'simplebar-react';
 
-import 'simplebar/dist/simplebar.min.css';
+import 'simplebar-react/dist/simplebar.min.css';
 import logo from './logo.svg';
 import './App.css';
+import Playground from './Playground';
 
 class App extends Component {
   render() {
@@ -16,11 +17,19 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <SimpleBar style={{ height: '300px' }}>
-          {[...Array(50)].map((x, i) =>
-            <p key={i} className="odd">Some content</p>
+        <Playground width height direction>
+          {({ height, width }) => (
+            <div style={{ width: '200px', height: '200px' }}>
+              <SimpleBar style={{ height, width }}>
+                {[...Array(50)].map((x, i) => (
+                  <p key={i} className="odd">
+                    Some content
+                  </p>
+                ))}
+              </SimpleBar>
+            </div>
           )}
-        </SimpleBar>
+        </Playground>
       </div>
     );
   }
