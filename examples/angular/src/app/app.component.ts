5cc03a6db02e8a1e6269a6612609f73fbcc2ff4a
Adrien Denat <oi@adriendenat.com>
2024-10-24 00:52:38 +0100

    chore: fix angular example

--- examples/angular/src/app/app.component.ts
+++ examples/angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
 import { SimplebarAngularModule } from 'simplebar-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SimplebarAngularModule],
+  imports: [RouterOutlet, SimplebarAngularModule, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
